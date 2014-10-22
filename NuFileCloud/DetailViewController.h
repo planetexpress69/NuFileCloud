2d3b616cc62134d847ad9d583289c4ddfcc83355
Martin Kautz <kautz@jakota.de>
2014-10-22 16:08:23 +0200

    Initial Commit

---
+++ NuFileCloud/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  NuFileCloud
+//
+//  Created by Martin Kautz on 22.10.14.
+//  Copyright (c) 2014 JAKOTA Design Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ NuFileCloud/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  NuFileCloud
+//
+//  Created by Martin Kautz on 22.10.14.
+//  Copyright (c) 2014 JAKOTA Design Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
